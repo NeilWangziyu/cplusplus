9c6299dd92fc811217fa5684af58dc5df5533319
Ziyu Wang <17210720158@fudan.edu.cn>
2019-06-28 15:36:06 +0800

    first commit

--- cplusplus/cplusplus/main.cpp
+++ cplusplus/cplusplus/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    int a, b;
+    int c;
+    a = 10;
+    b = 20;
+    c = a + b;
+    
+    cout << c <<endl;
+    
+    int area;
+    
+    area = LENGTH * WIDTH;
+    cout << area <<endl;
+    cout << NEWLINE;
+    
+    auto f = 3.14;
+    cout << f <<endl;
+    
+    
+    
     return 0;
 }
